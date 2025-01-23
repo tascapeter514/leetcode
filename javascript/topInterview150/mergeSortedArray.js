e099417d3dd83eeae96f56a884fe0da5aea9e794
petertasca <tascapeter@gmail.com>
2025-01-23 09:39:12 -0500

    merge sorted array javascript solution

--- javascript/topInterview150/mergeSortedArray.js
+++ javascript/topInterview150/mergeSortedArray.js
@@ -27,3 +27,42 @@
 // Explanation: The arrays we are merging are [] and [1].
 // The result of the merge is [1].
 // Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+
+
+// const testNums1 = [1,2,3,0,0,0];
+// const m = 3;
+// const testNums2 = [2,5,6]
+// const n = 3
+
+// const testNums1 = [1];
+// const m = 1;
+// const testNums2 = []
+// const n = 0;
+
+const testNums1 = [0];
+const m = 0;
+const testNums2 = [1];
+const n = 1;
+
+
+// MY SOLUTION
+const merge = function(nums1, m, nums2, n) {
+    if (!nums2) return nums1
+    for (let i = m; i < m + n; i++) {
+        const curr = nums2.shift()
+        nums1.fill(curr, i)
+    }
+    return nums1.sort((a, b) => a - b)
+    
+};
+
+console.log(merge(testNums1, m, testNums2, n))
+
+// LEETCODE SOLUTIONS
+
+// var merge = function(nums1, m, nums2, n) {
+//     for (let i = m, j = 0; j < n; i++, j++) {
+//         nums1[i] = nums2[j];
+//     }
+//     nums1.sort((a, b) => a - b);
+// };
