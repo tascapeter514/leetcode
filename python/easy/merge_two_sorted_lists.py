d3092d5e9444c4325c766a984b96553ce5de6829
petertasca <tascapeter@gmail.com>
2025-10-07 10:55:40 -0400

    feat: linked list recurse

--- python/easy/merge_two_sorted_lists.py
+++ python/easy/merge_two_sorted_lists.py
@@ -5,6 +5,7 @@ class ListNode(object):
         self.next = next
     def __repr__(self):
         return f"ListNode({self.val}, {repr(self.next)})"
+
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
         pass
@@ -14,16 +15,24 @@ def build_list(list = []):
     dummy = ListNode(0)
     curr = dummy
     for el in list:
-        print(el)
-        print('curr:', curr)
         node = ListNode(el)
         curr.next = node
         curr = curr.next
     return dummy.next
 
-    
-        
+
+def build_list_recurse(list = []):
+    print('list:', list)
+    if not list: return None
+    curr = ListNode(list[0])
+    print('current node:', curr)
+    curr.next = build_list_recurse(list[1:])
+    print('final curr:', curr)
+    return curr
+
 
 result = build_list([1, 2])
 
-print('result:', result)
+new_result = build_list_recurse([1, 2])
+
+print('result:', new_result)
